2d98b01062345ff4c78be0234263e9e077e80946
Alybee75 <alyana_erin_bondoc@dlsu.edu.ph>
2024-07-14 23:31:35 +0800

    best ckpts tfl

--- models.sh
+++ models.sh
@@ -8,3 +8,7 @@ cd ckpts
 mkdir -p phoenix-train-models
 wget --folder https://drive.google.com/drive/u/0/folders/1uNrXyTtEYhf658mVZ-mf7h773I5Myrf8 -O ./phoenix-train-models
 
+# Create directory for transfer learning models and download
+mkdir -p fsl-tfl-models
+wget --folder https://drive.google.com/drive/folders/1DHmVDcDqY8GajyCyeJxcoi5epJB6ctuw -O ./fsl-tfl-models
+
